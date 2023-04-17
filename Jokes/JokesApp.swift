7bf2fc7e4718a8c23713efa405fdcf59413ada3e
Jack Dellamea <jdellamea@lcs.on.ca>
2023-04-17 14:20:27 -0400

    Initial Commit

---
+++ Jokes/JokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokesApp.swift
+//  Jokes
+//
+//  Created by Jack Dellamea on 4/17/23.
+//
+
+import SwiftUI
+
+@main
+struct JokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
