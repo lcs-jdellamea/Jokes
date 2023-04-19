b1b954892829545819025c3587f52cf74fcc37bd
Jack Dellamea <jdellamea@lcs.on.ca>
2023-04-19 10:36:03 -0400

    Started to add view to see the list of saved / favourite jokes.

---
+++ Jokes/Views/FavoritesView.swift
@@ -0,0 +1,20 @@
+//
+//  FavoritesView.swift
+//  Jokes
+//
+//  Created by Jack Dellamea on 4/19/23.
+//
+
+import SwiftUI
+
+struct FavoritesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FavoritesView_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoritesView()
+    }
+}
